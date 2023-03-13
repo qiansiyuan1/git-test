88b783b6277099c7e89e671f9fddc803ea0d8a62
qiansiyuan <18352669350@163.com>
2023-03-13 21:39:17 +0800

    push-commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,5 +16,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("master-test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
